df08545dad16dd4c52767e94e233bdb640f56ce4
archy <archytigerman93@outlook.com>
2022-10-07 21:23:47 +0300

    мой коммит

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,88 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 1");
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
-        }
-        System.out.println("Задача 2");
-        for (int i = 10; i >= 0; i--) {
-            System.out.println(i);
-        }
-        System.out.println("Задача №3");
-        for (int i = 0; i <= 17; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i);
+
+                System.out.println("задача №1");
+                int deposit2 = 15000;
+                int total2 = 0;
+                int i = 0;
+                while(total2 <= 2_459_000){
+                    total2 = total2+deposit2;
+                    total2 = total2+total2/100;
+                    total2++;
+                    i++;
+                    System.out.println("месяц "+i+" сумма накоплений равна "+ total2);
+                }
+                System.out.println("Задача №2");
+                int a =0;
+                while(a!=10){
+                    a++;
+                    System.out.println(a);
+
+                }
+                for(a = 10; a>=0; a--){
+                    System.out.println(a);
+                }
+                System.out.println("Задача №3");
+                int totalPopulation = 12000000;
+                int fertility = 17;
+                int mortality = 8;
+                int dif = fertility - mortality;
+                for(int b = 1; b <= 10; b++){
+                    totalPopulation += totalPopulation * dif / 1000;
+                }
+                System.out.println("год 2032, численность населения составляет "+ totalPopulation);
+
+                System.out.println("Задача №3");
+                int deposit3 = 15000;
+                int total = 0;
+                for(int c = 0; total < 10_000_000; c++){
+                    total = total + total * 7/100;
+                    total = total + deposit3;
+                    System.out.println("месяц " + c + " итого " + total);
+                }
+
+                System.out.println("Задача №4");
+                int deposit4 = 15000;
+                int total3 = 0;
+                int c = 0;
+                for(; total3 < 10_000_000; c++){
+                    total3 = total3 + total3 * 7/100;
+                    total3 = total3 + deposit4;
+                    if(c % 6 == 0){
+                        System.out.println("месяц " + c + " итого " + total3);
+                    }
+                }
+                System.out.println("Задача №5");
+                int Friday = 1;
+                for(int numberOfMounth = 1; numberOfMounth <= 31; numberOfMounth++){
+                    if(Friday % numberOfMounth == 0){
+                        System.out.println("Сегодня пятница " + Friday +" число. Надо подготовить отчет");
+                        Friday +=7;
+                    }
+                }
+                System.out.println("Задача №6");
+                int startObservation = 1822;
+                int endObservation = 2122;
+                for(int t = 1822; t < 1922; t++){
+                    if(t % 79==0){
+                        System.out.println("Камета появилась в " + t + " году");
+                    }
+                }
+                for(int t = 1922; t < 2022; t++){
+                    if(t % 79 == 0){
+                        System.out.println("Камета появилась в " + t + " году");
+                    }
+                }
+                for(int t = 2022; t < 2122; t++){
+                    if(t % 79 == 0){
+                        System.out.println("Камета появилась в " + t + " году");
+                    }
+                }
+                System.out.println("Задача №7");
+                int num = 20;
+                for(int i2 = 2; i2 <= 20 ; i2=i2+2){
+                    System.out.println(i2);
+                }
             }
         }
-        System.out.println("Задча №4");
-        for (int i = 10; i >= -10; i--) {
-            System.out.println(i);
-        }
-        System.out.println("Задача №5");
-        for (int i = 1904; i <= 2096; i = i + 4) {
-            System.out.println("Год високосный " + i);
-        }
-        System.out.println("Задача №6");
-        for (int i = 7; i <= 98; i++) {
-            if (i % 7 == 0) {
-                System.out.println(i);
-            }
-        }
-        System.out.println("Задача №7");
-        for(int i = 1; i<=512;i=i*2){
-            System.out.println(i);
-        }
-        System.out.println("задача №8");
-        int deposit = 29000;
-        int total = 0;
-        for(int i = 1;i <= 12;i++){
-            total = total+deposit;
-            System.out.println("месяц "+i +" сумма накоплений равна "+ total);
-        }
-        System.out.println("задача №9");
-        int deposit2 = 29000;
-        int total2 = 0;
-        for(int i = 1;i <= 12;i++){
-            total2 = total2+total2/100;
-            total2 = total2+deposit2;
-            System.out.println("месяц "+i +" сумма накоплений равна "+ total2);
-        }
-    }
-}
+
